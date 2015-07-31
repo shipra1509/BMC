2eb30a33e08646e958d7dcf89a40e5b79dcfb202
Shipra Sinha <shipra.juet@gmail.com>
2015-07-31 11:14:37 +0530

    Store image of the user on authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150731044719) do
+ActiveRecord::Schema.define(version: 20150731053315) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20150731044719) do
     t.datetime "updated_at",                      null: false
     t.string   "provider"
     t.string   "uid"
+    t.string   "image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
